ad32e3f30ce33ffe0194e3ae1f74b816026b8df0
pbulbule <pallavi.bulbule25@gmail.com>
2019-08-02 16:54:59 +0530

    creating

---
+++ springboot-started/src/main/java/com/example/practice/springbootbasics/springbootstarted/SpringbootStartedApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice.springbootbasics.springbootstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartedApplication.class, args);
+	}
+
+}
---
+++ springboot-started/src/test/java/com/example/practice/springbootbasics/springbootstarted/SpringbootStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice.springbootbasics.springbootstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
